0813c0e83e5c38e322ba810d1ffeb05cc8e6afbd
Paulina Lichota <paulinalichota1@gmail.com>
2026-03-25 15:28:01 +0100

    Channel wip

--- Channel.cpp
+++ Channel.cpp
@@ -6,32 +6,164 @@
 /*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 17:56:28 by plichota          #+#    #+#             */
-/*   Updated: 2026/03/25 13:23:23 by plichota         ###   ########.fr       */
+/*   Updated: 2026/03/25 15:27:36 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Channel.hpp"
 
-Channel::Channel(std::string name) : _name(name), _topic(""), _password(""), _modes(), _operators(), _clients(), _maxUsers(-1)
-{
-}
+Channel::Channel(const std::string &name): 
+    _name(name),
+    _topic(""),
+    _key(""),
+    _inviteOnly(false),
+    _topicRestricted(false),
+    _usersLimit(0),
+    _operators(),
+    _members(),
+    _invited()
+{}
 
 Channel::~Channel()
-{
+{}
 
-}
+/* ------------------------------------ Info & Modes ----------------------------------- */
 
-std::string Channel::getName()
+const std::string &Channel::getName() const
 {
     return this->_name;
 }
 
+const std::string &Channel::getKey() const
+{
+    return this->_key;
+}
+
+void Channel::setKey(const std::string &key)
+{
+    this->_key = key;
+}
+
+bool Channel::getTopicRestricted() const
+{
+    return this->_topicRestricted;
+}
+
+void Channel::setTopicRestricted(bool restricted)
+{
+    this->_topicRestricted = restricted;
+}
+
+const std::string &Channel::getTopic() const
+{
+    return this->_topic;
+}
+
+void Channel::setTopic(const std::string& topic)
+{
+    this->_topic = topic;
+}
+
+bool Channel::getInviteOnly() const
+{
+    return this->_inviteOnly;
+}
+
+void Channel::setInviteOnly(bool inviteOnly)
+{
+    this->_inviteOnly = inviteOnly;
+}
+
+size_t Channel::getUsersLimit() const
+{
+    return this->_usersLimit;
+}
+
+void Channel::setUsersLimit(size_t limit)
+{
+    this->_usersLimit = limit;
+}
+
+/* ------------------------------------ Members ----------------------------------- */
+
+void Channel::addMember(const std::string &nickname)
+{
+    this->_members.insert(nickname);
+}
+
+void Channel::removeMember(const std::string &nickname)
+{
+    this->_members.erase(nickname);
+}
+
+bool Channel::isMember(const std::string &nickname) const
+{
+    return this->_members.find(nickname) != this->_members.end();
+}
+
+size_t Channel::getMemberCount() const
+{
+    return this->_members.size();
+}
+
+const std::set<std::string> &Channel::getMembers() const
+{
+    return this->_members;
+}
+
+/* ------------------------------------ Operators ----------------------------------- */
+
+void Channel::addOperator(const std::string &nickname)
+{
+    this->_operators.insert(nickname);
+}
+
+void Channel::removeOperator(const std::string &nickname)
+{
+    this->_operators.erase(nickname);
+}
+
+bool Channel::isOperator(const std::string &nickname) const
+{
+    return this->_operators.find(nickname) != this->_operators.end();
+}
+
+size_t Channel::getOperatorCount() const
+{
+    return this->_operators.size();
+}
+
+const std::set<std::string> &Channel::getOperators() const
+{
+    return this->_operators;
+}
+
+/* ------------------------------------ Invited ----------------------------------- */
+
+bool Channel::isInvited(const std::string &nickname) const
+{
+    return this->_invited.find(nickname) != this->_invited.end();
+}
+
+void Channel::addInvited(const std::string &nickname)
+{
+    this->_invited.insert(nickname);
+}
+
+void Channel::removeInvited(const std::string &nickname)
+{
+    this->_invited.erase(nickname);
+}
+
+/* ------------------------------------ Print ----------------------------------- */
+
+
+// da modificare dopo aver creato i getters
 void Channel::printChannelInfo()
 {
-	std::cout << "Channel: " << this->_name
-		<< ", Topic: " << this->_topic
-		<< ", Password: " << this->_password
-		<< ", client count: " << this->_clients.size()
-		<< ", operator count: " << this->_operators.size()
+	std::cout << "Channel: " << getName()
+		<< ", Topic: " << getTopic()
+		<< ", member count: " << getMemberCount()
+		<< ", operator count: " << getOperatorCount()
 		<< std::endl;
 }
--- Channel.hpp
+++ Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 17:55:39 by plichota          #+#    #+#             */
-/*   Updated: 2026/03/25 13:21:36 by plichota         ###   ########.fr       */
+/*   Updated: 2026/03/25 15:27:44 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,67 @@
 #include "Client.hpp"
 
 /*
-	All the messages sent from one client to a channel have to be forwarded to every other client that joined the channel.
+	· i: Set/remove Invite-only channel
+	· t: Set/remove the restrictions of the TOPIC command to channel operators
+	· k: Set/remove the channel key (password)
+	· o: Give/take channel operator privilege
+	· l: Set/remove the user limit to channel
 */
 class Channel
 {
 	private:
-		std::string _name;
-		std::string _topic;
-		std::string _password; // optional -k
-		std::set<char> _modes; // {i, t, k, o, l} the value itself acts as a key
-		/*
-			· i: Set/remove Invite-only channel
-			· t: Set/remove the restrictions of the TOPIC command to channel operators
-			· k: Set/remove the channel key (password)
-			· o: Give/take channel operator privilege
-			· l: Set/remove the user limit to channel
-		*/
-		std::set<int> _operators;	   // fd degli operatori
-		std::set<int> _clients;	  // fd di tutti i client connessi al canale
-		int _maxUsers;
+		const std::string	_name; // immutabile
+		std::string	_topic;
+		std::string	_key; 	// empty = no password
+		bool	_inviteOnly;	// +i: solo operatori possono invitare nuovi client, se un client prova ad unirsi senza essere stato invitato, ERR 473
+		bool	_topicRestricted; //  +t: solo operatori possono cambiare topic (ERR 482 se membro non operatore ci prova)
+		size_t		_usersLimit; // 0 = no limit, modificabile con MODE +l
+		std::set<std::string> _operators;	  // +o = proprietà degli utenti nicknames degli operatori (no fd perchè possono essere riassegnati)
+		std::set<std::string> _members;	  // nicknames di tutti i client connessi al canale (inclusi operatori)
+		std::set<std::string> _invited;	  // nicknames di tutti i client invitati
 
 	public:
-		Channel(std::string name);
+		Channel(const std::string &name);
 		~Channel();
 
-		// Getters
-		std::string getName();
+		// Info e modes
+		const std::string &getName() const; // name immutable, only getter
 
-		// Everyone
-		void propagateMessageToChannel(const std::string &message, Client sender); // invia message a tutti i client del canale tranne sender
+		const std::string &getKey() const;
+		void setKey(const std::string &key); // only op, se key è empty, rimuove la password
+
+		bool getTopicRestricted() const;
+		void setTopicRestricted(bool restricted); // only op
+	
+		const std::string &getTopic() const;
+		void setTopic(const std::string& topic); // if _topicRestricted, only op
+
+		bool getInviteOnly() const;
+		void setInviteOnly(bool inviteOnly); // only op
+		
+		size_t getUsersLimit() const;
+		void setUsersLimit(size_t limit); // only op
+
+		// Membri
+		void addMember(const std::string &nickname);
+		void removeMember(const std::string &nickname);
+		bool isMember(const std::string &nickname) const;
+		size_t getMemberCount() const;
+		const std::set<std::string> &getMembers() const;
+
+		// Operatori
+		void addOperator(const std::string &nickname); // only op, or first member that creates the channel
+		void removeOperator(const std::string &nickname); // only op
+		bool isOperator(const std::string &nickname) const; 
+		size_t getOperatorCount() const;
+		const std::set<std::string> &getOperators() const;
+
+		// Inviti
+		bool isInvited(const std::string &nickname) const;
+		void addInvited(const std::string &nickname); // if _inviteOnly, only op can add invited
+		void removeInvited(const std::string &nickname); // rimuovi dopo il JOIN
 
 		void printChannelInfo();
-		// Operators
-		void create(Client client);	// crea canale se non esiste, se il canale è protetto da password, client deve fornire la password corretta
-		void join(Client client);			// aggiunge un client al canale
-		void kick(Client client);		  // kick un client dal canale
-		void invite(Client client);		  // invita un client al canale
-		void setTopic(std::string topic); // setta il topic del canale
-		void setMode(char mode);		  // setta un mode del canale // +- operatore
 };
 
 #endif
